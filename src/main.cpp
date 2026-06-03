0f6b1f7430087f8cf2124705d804f6c8e50f1156
Harshavardhan Ravichander <harshavardhan.r.misc@gmail.com>
2026-06-03 16:22:09 +0000

    Giving the Circle Velocity

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,24 @@ using namespace sf;
 
 int main() {
 
-	RenderWindow window(VideoMode(), "Drawing a circle", State::Fullscreen);
+	//setup
+
+	//RenderWindow window(VideoMode::getDesktopMode(), "Drawing a circle", State::Fullscreen);
+	RenderWindow window(VideoMode({1600, 900}), "Drawing a circle");
 	float height = window.getSize().x;
 	float width = window.getSize().y;
 
 	CircleShape circle(100.f);
 	circle.setOrigin({100.f, 100.f});
-	circle.setPosition({height/2, width/2});
 	circle.setFillColor(Color::Blue);
 
+	float circleXpos = height/2, circleYpos = width/2; 	//initial position
+	float circleXvel = 300.f, circleYvel = -500.f; 			//initial velocity
+
+	Clock clock; 				//start timing
+
+	Time time1 = clock.getElapsedTime(), time2, Dtime; 	//Dtime is (Delta) change in time
+	
 	while(window.isOpen()) {
 
 		while(const std::optional event = window.pollEvent()) {
@@ -24,8 +33,21 @@ int main() {
 			}
 		}
 
-		window.clear(Color::Black);
-		window.draw(circle); //draw stuff
+		//----DISPLAY CYCLE----
+
+		//clear screen
+		window.clear(Color::Black); 
+
+		//draw object
+		circle.setPosition({circleXpos, circleYpos});
+		window.draw(circle); 
+
+		//update object
+		time2 = clock.getElapsedTime();
+		Dtime = time2-time1;
+		time1 = time2;
+		circleXpos+=circleXvel*Dtime.asSeconds(); circleYpos+=circleYvel*Dtime.asSeconds();
+
 		window.display();
 
 	}
